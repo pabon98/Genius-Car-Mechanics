f3651a83c04198a6ec2f39014b61b6a2be5b7f78
pabon98 <hasinanjumpabon@gmail.com>
2021-10-15 21:54:40 +0600

    car-mechanics added

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBd3F0zsbl_WUqVDcFUr_uzefTzmq7YWfw",
+    authDomain: "genius-car-mechanics-f6492.firebaseapp.com",
+    projectId: "genius-car-mechanics-f6492",
+    storageBucket: "genius-car-mechanics-f6492.appspot.com",
+    messagingSenderId: "760320548097",
+    appId: "1:760320548097:web:72549417cf538e19d84f00"
+  };
+
+export default firebaseConfig;
